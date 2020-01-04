d39392ac8951b8ab2b6221846b9fe0058f33c551
aditya1290 <adityagarg1290@gmail.com>
2020-01-05 04:14:26 +0530

    QR Scanner Completed ( Camera Settings fixed ) QR Generator completed ( Image can be successfully saved in device )
    Backend : returns in dialog
    Frontend : Not done

--- app/src/main/java/com/example/sih2020/MainActivity.java
+++ app/src/main/java/com/example/sih2020/MainActivity.java
@@ -2,13 +2,42 @@ package com.example.sih2020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button scan;
+    Button generate;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        scan =(Button)findViewById(R.id.scan);
+        generate = (Button)findViewById(R.id.generate);
+
+        scan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, ScanQRActivity.class);
+                startActivity(i);
+            }
+        });
+
+        generate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, GenerateQRActivity.class);
+                startActivity(i);
+            }
+        });
+
+
+
     }
 }
