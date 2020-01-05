1bd0ab3d21b37f7a18dc33f14bc7f12012af4f68
aditya1290 <adityagarg1290@gmail.com>
2020-01-05 17:04:32 +0530

    QR Scanner Completed ( Camera Settings fixed ) QR Generator completed ( Image can be successfully saved in device )
    Backend : returns in dialog
    Frontend : Not done

---
+++ app/src/main/java/com/example/sih2020/GenerateQRActivity.java
@@ -0,0 +1,173 @@
+package com.example.sih2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class GenerateQRActivity extends AppCompatActivity {
+
+
+    EditText input;
+    ImageView qrcode;
+    ImageView image;
+    Button GenerateQR;
+    String text;
+    Button save;
+    OutputStream outputStream;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_generate_qr);
+
+        GenerateQR = (Button)findViewById(R.id.generate);
+        input = (EditText)findViewById(R.id.qrtext);
+        qrcode = (ImageView)findViewById(R.id.qrcode);
+        save = (Button)findViewById(R.id.save);
+
+        //Permission
+        if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+        {
+            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+        }
+        if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+        {
+            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},1);
+        }
+
+        GenerateQR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text = input.getText().toString().trim();
+                MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+                try{
+                    BitMatrix bitMatrix = multiFormatWriter.encode(text, BarcodeFormat.QR_CODE,200,200);
+                    BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+                    Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+                    qrcode.setImageBitmap(bitmap);
+
+                }catch(WriterException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveImage();
+            }
+        });
+    }
+
+    private void saveImage() {
+
+        ImageView img = (ImageView) findViewById(R.id.qrcode);
+        BitmapDrawable draw = (BitmapDrawable) img.getDrawable();
+        Bitmap bitmap = draw.getBitmap();
+        FileOutputStream outStream = null;
+        File sdCard = Environment.getExternalStorageDirectory();
+        File dir = new File(sdCard.getAbsolutePath() + "/QRcode");
+        dir.mkdirs();
+        File outFile = new File(dir, text+".jpg");
+        try {
+            outStream = new FileOutputStream(outFile);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outStream);
+        Toast.makeText(this, "Image Saved Successfully", Toast.LENGTH_SHORT).show();
+        try {
+            outStream.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            outStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Some extra code to use further
+// 1. Dialog box for QR code
+//Starting of dialog box
+/*
+    public void myAlertbox()
+    {
+        MyDialog = new Dialog(GenerateQRActivity.this);
+        MyDialog.setContentView(R.layout.qrcode_dialog);
+        MyDialog.setTitle("QR CODE");
+        save = (Button)findViewById(R.id.dialogSave);
+        exit = (Button)findViewById(R.id.dialogExit);
+        ImageinDialog = (ImageView)findViewById(R.id.ImageInDialog);
+        ImageinDialog.setImageDrawable(getResources().getDrawable(R.drawable.bhj));
+
+        save.setEnabled(true);
+        exit.setEnabled(true);
+
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MyDialog.cancel();
+            }
+        });
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(GenerateQRActivity.this, "Save your image here", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+    *///------>Ending
---
+++ app/src/main/java/com/example/sih2020/ScanQRActivity.java
@@ -0,0 +1,166 @@
+package com.example.sih2020;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcode;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetector;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetectorOptions;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+import com.karumi.dexter.listener.single.PermissionListener;
+import com.otaliastudios.cameraview.CameraView;
+import com.otaliastudios.cameraview.frame.Frame;
+import com.otaliastudios.cameraview.frame.FrameProcessor;
+
+import java.util.List;
+
+public class ScanQRActivity extends AppCompatActivity {
+
+    CameraView cameraView;
+    boolean isDetected = false;
+
+    FirebaseVisionBarcodeDetector detector;
+    FirebaseVisionBarcodeDetectorOptions options;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan_qr);
+
+        if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+        {
+            requestPermissions(new String[]{Manifest.permission.CAMERA},1);
+        }
+        if (checkSelfPermission(Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED)
+        {
+            requestPermissions(new String[]{Manifest.permission.RECORD_AUDIO},1);
+        }
+
+
+        Dexter.withActivity(this)
+                .withPermissions(new String[]{Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO})
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport report) {
+                        setupCamera();
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+
+                    }
+                }).check();
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 1);
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)!= PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, 1);
+        }
+    }
+
+    private void setupCamera()
+    {
+        cameraView = (CameraView)findViewById(R.id.CameraView);
+        cameraView.setLifecycleOwner(this);
+        cameraView.addFrameProcessor(new FrameProcessor() {
+            @Override
+            public void process(@NonNull Frame frame) {
+                processImage(getVisionImageFromFrame(frame));
+            }
+        });
+
+
+        options = new FirebaseVisionBarcodeDetectorOptions.Builder()
+                .setBarcodeFormats(FirebaseVisionBarcode.FORMAT_QR_CODE).build();
+
+        detector = FirebaseVision.getInstance().getVisionBarcodeDetector(options);
+    }
+
+     private void processImage(FirebaseVisionImage image) {
+        if(!isDetected)
+        {
+            detector.detectInImage(image)
+                    .addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionBarcode>>() {
+                        @Override
+                        public void onSuccess(List<FirebaseVisionBarcode> firebaseVisionBarcodes) {
+                            processResult(firebaseVisionBarcodes);
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(ScanQRActivity.this,""+ e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        }
+    }
+
+    private FirebaseVisionImage getVisionImageFromFrame(Frame frame)
+    {
+        byte[] data = frame.getData();
+        FirebaseVisionImageMetadata metadata = new FirebaseVisionImageMetadata.Builder()
+                .setFormat(FirebaseVisionImageMetadata.IMAGE_FORMAT_NV21)
+                .setWidth(frame.getSize().getWidth())
+                .setHeight(frame.getSize().getHeight())
+                .build();
+
+        return FirebaseVisionImage.fromByteArray(data, metadata);
+    }
+
+    private void processResult(List<FirebaseVisionBarcode> firebaseVisionBarcodes)
+    {
+        if(firebaseVisionBarcodes.size()>0)
+        {
+            for(FirebaseVisionBarcode item : firebaseVisionBarcodes)
+            {
+                createDialog(item.getRawValue());
+            }
+
+        }
+    }
+    private void createDialog(String text)                      //creatint UI for Dialog box which complete on ending current activity as well
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        isDetected = !isDetected;
+        builder.setMessage(text)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+                        dialogInterface.dismiss();
+                        finish();
+                    }
+                });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+
+
+}
