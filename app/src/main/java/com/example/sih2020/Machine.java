a2bc82869fc69eed29e6951dd75ea6efd057f83c
vikas <goyalvikas546@gmail.com>
2020-01-05 14:12:12 +0530

    machine and pastrecord classes

---
+++ app/src/main/java/com/example/sih2020/Machine.java
@@ -0,0 +1,75 @@
+package com.example.sih2020;
+
+import java.net.URI;
+import java.util.Date;
+import java.util.List;
+
+public class Machine {
+    String serialNo;
+    Date installationDate;
+    String department;
+    int serviceTime;
+    URI QRImage;
+    List<PastRecord> pastRecordList;
+
+    public Machine (){
+
+    }
+
+    public Machine(String serialNo, Date installationDate, String department, int serviceTime, URI QRImage, List<PastRecord> pastRecordList) {
+        this.serialNo = serialNo;
+        this.installationDate = installationDate;
+        this.department = department;
+        this.serviceTime = serviceTime;
+        this.QRImage = QRImage;
+        this.pastRecordList = pastRecordList;
+    }
+
+    public String getSerialNo() {
+        return serialNo;
+    }
+
+    public Date getInstallationDate() {
+        return installationDate;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public int getServiceTime() {
+        return serviceTime;
+    }
+
+    public URI getQRImage() {
+        return QRImage;
+    }
+
+    public List<PastRecord> getPastRecordList() {
+        return pastRecordList;
+    }
+
+    public void setSerialNo(String serialNo) {
+        this.serialNo = serialNo;
+    }
+
+    public void setInstallationDate(Date installationDate) {
+        this.installationDate = installationDate;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public void setServiceTime(int serviceTime) {
+        this.serviceTime = serviceTime;
+    }
+
+    public void setQRImage(URI QRImage) {
+        this.QRImage = QRImage;
+    }
+
+    public void setPastRecordList(List<PastRecord> pastRecordList) {
+        this.pastRecordList = pastRecordList;
+    }
+}
---
+++ app/src/main/java/com/example/sih2020/PastRecord.java
@@ -0,0 +1,53 @@
+package com.example.sih2020;
+
+import java.util.Date;
+
+public class PastRecord {
+    Date serviceDate;
+    String serviceMan;
+    String Description;
+    boolean isDone;
+
+    public PastRecord() {
+
+    }
+
+    public PastRecord(Date serviceDate, String serviceMan, String description, boolean isDone) {
+        this.serviceDate = serviceDate;
+        this.serviceMan = serviceMan;
+        Description = description;
+        this.isDone = isDone;
+    }
+
+    public Date getServiceDate() {
+        return serviceDate;
+    }
+
+    public String getServiceMan() {
+        return serviceMan;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setServiceDate(Date serviceDate) {
+        this.serviceDate = serviceDate;
+    }
+
+    public void setServiceMan(String serviceMan) {
+        this.serviceMan = serviceMan;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public void setDone(boolean done) {
+        isDone = done;
+    }
+}
