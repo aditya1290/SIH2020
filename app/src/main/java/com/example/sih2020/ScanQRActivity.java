5837159d2c42654b16daeeb9ec6a76f1001842ce
aditya1290 <adityagarg1290@gmail.com>
2020-01-07 21:24:00 +0530

    Revert "Nothing done"
    This reverts commit 15c1ba2f

--- app/src/main/java/com/example/sih2020/ScanQRActivity.java
+++ app/src/main/java/com/example/sih2020/ScanQRActivity.java
@@ -41,6 +41,7 @@ import com.otaliastudios.cameraview.CameraView;
 import com.otaliastudios.cameraview.frame.Frame;
 import com.otaliastudios.cameraview.frame.FrameProcessor;
 
+import java.io.IOException;
 import java.util.List;
 
 public class ScanQRActivity extends AppCompatActivity {
@@ -69,10 +70,10 @@ public class ScanQRActivity extends AppCompatActivity {
 
 
 
-
         Dexter.withActivity(this)
                 .withPermissions(new String[]{Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO})
                 .withListener(new MultiplePermissionsListener() {
+
                     @Override
                     public void onPermissionsChecked(MultiplePermissionsReport report) {
                         setupCamera();
@@ -98,13 +99,14 @@ public class ScanQRActivity extends AppCompatActivity {
     {
         cameraView = (CameraView)findViewById(R.id.CameraView);
         cameraView.setLifecycleOwner(this);
+
         cameraView.addFrameProcessor(new FrameProcessor() {
             @Override
             public void process(@NonNull Frame frame) {
                 processImage(getVisionImageFromFrame(frame));
+
             }
         });
-
 
         options = new FirebaseVisionBarcodeDetectorOptions.Builder()
                 .setBarcodeFormats(FirebaseVisionBarcode.FORMAT_QR_CODE).build();
@@ -120,6 +122,7 @@ public class ScanQRActivity extends AppCompatActivity {
                     .addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionBarcode>>() {
                         @Override
                         public void onSuccess(List<FirebaseVisionBarcode> firebaseVisionBarcodes) {
+
                             processResult(firebaseVisionBarcodes);
                         }
                     })
@@ -127,8 +130,10 @@ public class ScanQRActivity extends AppCompatActivity {
                         @Override
                         public void onFailure(@NonNull Exception e) {
                             Toast.makeText(ScanQRActivity.this,""+ e.getMessage(), Toast.LENGTH_SHORT).show();
+
                         }
                     });
+
         }
     }
 
@@ -140,6 +145,14 @@ public class ScanQRActivity extends AppCompatActivity {
                 .setWidth(frame.getSize().getWidth())
                 .setHeight(frame.getSize().getHeight())
                 .build();
+
+
+        try {
+            detector.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
         return FirebaseVisionImage.fromByteArray(data, metadata);
     }
