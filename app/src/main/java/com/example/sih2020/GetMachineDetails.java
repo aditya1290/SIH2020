c69aa0ed414d0cf0f60c31b9bc1ed3206c78c688
Sudhanshu Gupta <sudhanshu10101999@gmail.com>
2020-01-11 12:42:01 +0530

    user database added

--- app/src/main/java/com/example/sih2020/GetMachineDetails.java
+++ app/src/main/java/com/example/sih2020/GetMachineDetails.java
@@ -1,18 +1,21 @@
 package com.example.sih2020;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.example.sih2020.model.Complaint;
+import com.example.sih2020.serviceMan.ServiceMan;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -20,18 +23,29 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
-import java.net.URL;
+import java.util.Date;
+import java.util.HashMap;
 
 public class GetMachineDetails extends AppCompatActivity {
 
     FirebaseDatabase firebaseDatabase;
-    DatabaseReference machineReference;
+    DatabaseReference machineReference, complaintId, serviceManListReference;
+
+    FirebaseAuth auth;
+    FirebaseUser user;
 
     String generationCode;
     Machine machine;
 
     ImageView QRCodeImage;
     Button show_history;
+
+    Complaint complaint;
+
+    String complaintIdValue;
+
+    Button generateComplaint;
+    HashMap<String,ServiceMan> serviceManList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,12 +54,18 @@ public class GetMachineDetails extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        generateComplaint = findViewById(R.id.generateComplaint);
         show_history = findViewById(R.id.show_history);
 
         generationCode = getIntent().getStringExtra("generationCode");
 
         firebaseDatabase = FirebaseDatabase.getInstance();
         machineReference = firebaseDatabase.getReference("machines").child(generationCode);
+        complaintId = firebaseDatabase.getReference("complaintId");
+        serviceManListReference = firebaseDatabase.getReference("Users").child("ServiceMan");
+
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
 
         QRCodeImage = findViewById(R.id.QrCodeImage);
 
@@ -71,6 +91,56 @@ public class GetMachineDetails extends AppCompatActivity {
                 Intent i = new Intent(GetMachineDetails.this, ShowHistory.class);
                 i.putExtra("generationCode",generationCode);
                 startActivity(i);
+            }
+        });
+
+        generateComplaint.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                complaintId.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                        complaintIdValue = dataSnapshot.getValue().toString();
+                        complaint = new Complaint();
+                        complaint.setComplaintGenerator(user.getUid());
+                        complaint.setComplaintMachineId(generationCode);
+                        complaint.setComplaintGeneratedDate(new Date(2019,1,11));
+                        complaint.setStatus(complaint.getGeneratedOnly());
+
+
+                        serviceManListReference.addValueEventListener(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                                for(DataSnapshot serviceMan : dataSnapshot.getChildren())
+                                {
+                                    String key = serviceMan.getKey();
+
+
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                            }
+                        })  ;
+
+
+
+                    }
+
+
+
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
             }
         });
 
