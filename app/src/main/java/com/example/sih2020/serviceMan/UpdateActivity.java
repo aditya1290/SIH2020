5ba39461175ce5afa6007fa1dd1d53f7882d6388
vikas <goyalvikas546@gmail.com>
2020-01-11 19:59:25 +0530

    pending complaints for serviceman

--- app/src/main/java/com/example/sih2020/serviceMan/UpdateActivity.java
+++ app/src/main/java/com/example/sih2020/serviceMan/UpdateActivity.java
@@ -1,31 +1,41 @@
 package com.example.sih2020.serviceMan;
 
-import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.sih2020.R;
+import com.example.sih2020.model.Request;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class UpdateActivity extends AppCompatActivity {
 
 
     FirebaseDatabase firebaseDatabase;
-    DatabaseReference RequestIdReference, RequestReference;
+    DatabaseReference requestIdReference, requestReference;
 
     FirebaseAuth auth;
     FirebaseUser user;
 
     EditText Submit_Description;
     RadioGroup radio_group;
+    RadioButton radioButton;
+    String status;
     Button submit_update;
+
+    String requestIdValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +47,41 @@ public class UpdateActivity extends AppCompatActivity {
         radio_group = (RadioGroup) findViewById(R.id.radio_group);
         submit_update = (Button) findViewById(R.id.submit_update);
 
-        submit_update.setOnClickListener(new OnClickListener());
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        requestIdReference = firebaseDatabase.getReference("ReuestId");
+        requestReference = firebaseDatabase.getReference("Requests");
+
+
+        requestIdReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                requestIdValue = dataSnapshot.getValue().toString();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        })
+
+
+        submit_update.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                int selectedId = radio_group.getCheckedRadioButtonId();
+                radioButton = (RadioButton) findViewById(selectedId);
+                status = radioButton.getText().toString();
+
+                Request request = new Request();
+                request.setServiceMan(user.getUid());
+
+
+
+            }
+        });
 
         
     }
