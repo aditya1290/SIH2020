2e0e9436bcca0e8de640b5079277e1e903bd87a9
Abhinendra <abhinendragaurav@gmail.com>
2020-01-13 00:04:23 +0530

    cards added

--- app/src/main/java/com/example/sih2020/serviceMan/PendingComplaintsActivity.java
+++ app/src/main/java/com/example/sih2020/serviceMan/PendingComplaintsActivity.java
@@ -1,7 +1,11 @@
 package com.example.sih2020.serviceMan;
 
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -112,7 +116,42 @@ public class PendingComplaintsActivity extends AppCompatActivity {
 
 
     }
+    public class DividerItemDecoration extends RecyclerView.ItemDecoration{
 
+        private Drawable mDivider;
+
+        public DividerItemDecoration(Drawable divider) {
+            mDivider = divider;
+        }
+        @Override
+        public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+            super.getItemOffsets(outRect, view, parent, state);
+
+            if (parent.getChildAdapterPosition(view) == 0) {
+                return;
+            }
+
+            outRect.top = mDivider.getIntrinsicHeight();
+        }
+        @Override
+        public void onDraw(Canvas canvas, RecyclerView parent, RecyclerView.State state) {
+            int dividerLeft = parent.getPaddingLeft();
+            int dividerRight = parent.getWidth() - parent.getPaddingRight();
+
+            int childCount = parent.getChildCount();
+            for (int i = 0; i < childCount - 1; i++) {
+                View child = parent.getChildAt(i);
+
+                RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child.getLayoutParams();
+
+                int dividerTop = child.getBottom() + params.bottomMargin;
+                int dividerBottom = dividerTop + mDivider.getIntrinsicHeight();
+
+                mDivider.setBounds(dividerLeft, dividerTop, dividerRight, dividerBottom);
+                mDivider.draw(canvas);
+            }
+        }
+    }
 
 
 }
