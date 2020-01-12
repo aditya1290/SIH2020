dab16d5f5937d4578490ea0367df511eeda3ddd2
aditya1290 <adityagarg1290@gmail.com>
2020-01-12 11:26:09 +0530

    serviceman_pending_request

--- app/src/main/java/com/example/sih2020/serviceMan/RequestCompleteAdapter.java
+++ app/src/main/java/com/example/sih2020/serviceMan/RequestCompleteAdapter.java
@@ -1,10 +1,23 @@
 package com.example.sih2020.serviceMan;
 
+import android.view.View;
+
 import androidx.recyclerview.widget.RecyclerView;
 
 public class RequestCompleteAdapter extends RecyclerView.Adapter<RequestCompleteAdapter.myHolder1> {
 
 
 
+
+    public class myHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+
+
+
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
 
 }
