e138fe67b57f9960271fce11712e9fea7c91caaf
aditya1290 <adityagarg1290@gmail.com>
2020-01-12 11:16:59 +0530

    serviceman_pending_request

--- app/src/main/java/com/example/sih2020/serviceMan/RequestPending.java
+++ app/src/main/java/com/example/sih2020/serviceMan/RequestPending.java
@@ -4,6 +4,7 @@ package com.example.sih2020.serviceMan;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
@@ -30,9 +31,17 @@ public class RequestPending extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
+        s_recyclerView_pending_request = container.findViewById(R.id.s_recyclerView_pending_request);
+        s_recyclerView_pending_request.setLayoutManager(new LinearLayoutManager(this));
+
+
+        myRequestPendingAdapter = new RequestPendingAdapter(this, getmylist());
+        s_recyclerView_pending_request.setAdapter(myRequestPendingAdapter);
+
+
         return inflater.inflate(R.layout.serviceman_request_pending, container, false);
     }
 
-
-
 }
+
