874457e3c09918f78210e952ecd73f5684703808
Ankit_Goyal <goyala960@gmail.com>
2020-01-18 16:57:39 +0530

    setting and feedback

--- app/src/main/java/com/example/inventory/responsibleMan/ProfileFragment.java
+++ app/src/main/java/com/example/inventory/responsibleMan/ProfileFragment.java
@@ -1,6 +1,7 @@
 package com.example.inventory.responsibleMan;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,14 +9,18 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.inventory.R;
+import com.example.inventory.SettingActivity;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ProfileFragment extends Fragment {
 
+
+    ImageView setting_imegeView;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -26,7 +31,19 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+
+        View view= inflater.inflate(R.layout.fragment_profile, container, false);
+        setting_imegeView=view.findViewById(R.id.img_setting);
+        setting_imegeView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getActivity().getApplicationContext(), SettingActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        return view;
     }
 
 }
